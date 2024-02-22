069449dce12b699a0ad6a8c5abc6383723b4e135
kahobusiness <30713603+kahobusiness@users.noreply.github.com>
2024-02-22 20:09:52 +0800

    Made Gallery Pages Generated Static

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -1,6 +1,5 @@
 //这是一个客户端组件，通过 fetch 调用 /pages/api/images.tsx 中的 API，获取指定的图片数据
 
-import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import styles from './gallery.module.css'
 
@@ -13,35 +12,7 @@ interface Image {
   src: string;
 }
 
-const Gallery: React.FC<{ filePath: string }> = ({ filePath }) => {
-  const [images, setImages] = useState<Image[]>([]);
-
-  useEffect(() => {
-    const fetchImages = async () => {
-      try {
-
-        // 在 URL 中包含查询参数
-        const url = new URL('/api/images', window.location.origin);
-        url.searchParams.append('filePath', filePath);
-
-        // 调取 API 获取图片
-        const response = await fetch(url.toString());
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-
-        if (data.images && Array.isArray(data.images)) {
-          setImages(data.images); // 确保这里的data.images是ImageData[]类型
-        }
-      } catch (error) {
-        console.error("Fetching images failed", error);
-      }
-    };
-
-    fetchImages();
-  }, [filePath]);
+const Gallery: React.FC<{ images: Image[] }> = ({ images }) => {
 
   const getAltFromSrc = (src) => {
     const parts = src.split('/');
@@ -72,8 +43,6 @@ const Gallery: React.FC<{ filePath: string }> = ({ filePath }) => {
               alt={`Image: ${getAltFromSrc(image.src)}`}
               width={600}
               height={400}
-              placeholder='blur'
-              blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mM8IQkAAa8A48opxD0AAAAASUVORK5CYII='
               style={{ maxWidth: '100%', height: 'auto', cursor: 'pointer' }}
             />
           </a>
---
+++ getImagesSrc.js
@@ -0,0 +1,35 @@
+//终端输入 node getImagesSrc.js 运行本脚本，获取图片地址数组
+//修改 filePath，以获取不同目录下的图片
+
+const filePath = 'public/japan-journey'
+
+//以下脚本正文，无需修改
+
+const fs = require('fs');
+const path = require('path');
+
+// 设定目标目录（相对于此脚本文件的位置）
+const targetDirectory = path.join(__dirname, filePath);
+
+// 读取目录下的所有文件
+fs.readdir(targetDirectory, (err, files) => {
+  if (err) {
+    console.error('Could not list the directory.', err);
+    process.exit(1);
+  }
+
+  // 过滤文件列表以包含特定类型的文件，例如只有 jpg 图片
+  const imageFiles = files.filter(file => path.extname(file).toLowerCase() === '.jpg');
+
+  // 构建 images 数组
+  const images = imageFiles.map(file => ({
+    src: `/japan-journey/${file}`
+  }));
+
+  // 将数组转换为字符串并打印出来
+  console.log('export const images = [');
+  images.forEach(image => {
+    console.log(`  { src: '${image.src}' },`);
+  });
+  console.log('];');
+});
