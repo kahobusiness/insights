72a2e28ddc9643116ec855f064048f4199af53d7
Kaho <kaho.business@gmail.com>
2025-06-27 09:52:43 +0800

    Merge all chinese docs

--- content/zh/_meta.js
+++ content/zh/_meta.js
@@ -1,6 +1,17 @@
 export default {
   say_hello: '👋 你好，世界！',
   logs: '📔 更新日志',
+  'thoughts': {
+    type: 'separator',
+    title: '产品思维'
+  },
+  ai_era: ' AI 时代',
+  user: '用户思维',
+  commercialization: '商业化',
+  'journey': {
+    type: 'separator',
+    title: '旅行'
+  },
   japan_journey: '🇯🇵 端午日本游记',
   japan_gallery: {
     type: 'page',
---
+++ content/zh/user/_meta.js
@@ -0,0 +1,4 @@
+export default {
+    "the-7-question": "产品七问",
+    "user-value": "用户价值"
+}
